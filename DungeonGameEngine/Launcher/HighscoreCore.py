aa88052ee38db4171ea6a16b438ca45f9b6f8367
jakubwida <jakub.wida@outlook.com>
2018-02-26 16:16:57 +0100

    addded fallback for python 3.2

--- DungeonGameEngine/Launcher/HighscoreCore.py
+++ DungeonGameEngine/Launcher/HighscoreCore.py
@@ -11,8 +11,27 @@ class HighscoreCore:
 
 	def get_scores(self,order,time,username=""):
 
-		result = subprocess.run([self.dir_path+"/HighscoreServer/HighscoreDatabase.sh get "+ str(order)+" "+str(time)+" "+str(username)], stdout=subprocess.PIPE, shell=True)
+		result = self.run([self.dir_path+"/HighscoreServer/HighscoreDatabase.sh get "+ str(order)+" "+str(time)+" "+str(username)], stdout=subprocess.PIPE, shell=True)
 
 		list_of_results = str(result.stdout.decode('utf-8')).splitlines()
 
 		return list_of_results
+
+	def run(*popenargs, input=None, check=False, **kwargs):
+	    if input is not None:
+	        if 'stdin' in kwargs:
+	            raise ValueError('stdin and input arguments may not both be used.')
+	        kwargs['stdin'] = subprocess.PIPE
+
+	    process = subprocess.Popen(*popenargs, **kwargs):
+	    try:
+	        stdout, stderr = process.communicate(input)
+	    except:
+	        process.kill()
+	        process.wait()
+	        raise
+	    retcode = process.poll()
+	    if check and retcode:
+	        raise subprocess.CalledProcessError(
+	            retcode, process.args, output=stdout, stderr=stderr)
+	    return retcode, stdout, stderr
